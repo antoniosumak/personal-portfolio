d832ec3f084e4b25e0980345111b8c92e49238ab
Antonio Šumak <sumakantonio@gmail.com>
2023-03-14 22:04:15 +0100

    Add basic components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#443371',
+        secondary: '#D15875',
+        accent: '#F98465',
+      },
+    },
+  },
+  plugins: [],
+};
